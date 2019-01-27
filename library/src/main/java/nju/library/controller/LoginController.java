6b55334a2f7b14cef0791171902a4ddfbcf2e5aa
kiki <798838812@qq.com>
2019-01-27 09:00:27 +0800

    add controller

--- library/src/main/java/nju/library/controller/LoginController.java
+++ library/src/main/java/nju/library/controller/LoginController.java
@@ -3,11 +3,13 @@ package nju.library.controller;
 import com.alibaba.fastjson.JSONObject;
 import nju.library.entity.User;
 import nju.library.factory.DaoFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 @Controller
@@ -21,12 +23,27 @@ public class LoginController {
         String userId = jsonParam.getString("userId");
         System.out.println(userId);
 
+        String password = jsonParam.getString("userId");
+        System.out.println(password);
+
         User user = DaoFactory.getUserDao().getUserById(userId);
         // 将获取的json数据封装一层，然后在给返回
         JSONObject result = new JSONObject();
         result.put("msg", "ok");
         result.put("method", "json");
-        result.put("data", jsonParam);
+//        result.put("data", jsonParam);
+
+        return result.toJSONString();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "user/login2", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public String login2(){
+        // 将获取的json数据封装一层，然后在给返回
+        JSONObject result = new JSONObject();
+        result.put("msg", "ok");
+        result.put("method", "json");
+//        result.put("data", jsonParam);
 
         return result.toJSONString();
     }
